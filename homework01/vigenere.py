eb3189b7a2a440721f0589130a88c6733633123c
unknown <Nastenkla@gmail.com>
2025-11-16 20:31:12 +0300

    Реализованo vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -13,11 +13,11 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     for k, elem in enumerate(plaintext):
         if elem.isalpha():
             if 97 <= ord(elem) <= 122:
-                shift = ord(keyword[k % len(keyword)]) - ord('a')
-                ciphertext += chr((ord(elem) - ord('a') + shift + 26) % 26 + ord('a'))
+                shift = ord(keyword[k % len(keyword)]) - ord("a")
+                ciphertext += chr((ord(elem) - ord("a") + shift + 26) % 26 + ord("a"))
             elif 65 <= ord(elem) <= 90:
-                shift = ord(keyword[k % len(keyword)]) - ord('A')
-                ciphertext += chr((ord(elem) - ord('A') + shift + 26) % 26 + ord('A'))
+                shift = ord(keyword[k % len(keyword)]) - ord("A")
+                ciphertext += chr((ord(elem) - ord("A") + shift + 26) % 26 + ord("A"))
         else:
             ciphertext += elem
     return ciphertext
@@ -38,11 +38,11 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     for k, elem in enumerate(ciphertext):
         if elem.isalpha():
             if 97 <= ord(elem) <= 122:
-                shift = ord(keyword[k % len(keyword)]) - ord('a')
-                plaintext += chr((ord(elem) - ord('a') - shift + 26) % 26 + ord('a'))
+                shift = ord(keyword[k % len(keyword)]) - ord("a")
+                plaintext += chr((ord(elem) - ord("a") - shift + 26) % 26 + ord("a"))
             elif 65 <= ord(elem) <= 90:
-                shift = ord(keyword[k % len(keyword)]) - ord('A')
-                plaintext += chr((ord(elem) - ord('A') - shift + 26) % 26 + ord('A'))
+                shift = ord(keyword[k % len(keyword)]) - ord("A")
+                plaintext += chr((ord(elem) - ord("A") - shift + 26) % 26 + ord("A"))
         else:
             plaintext += elem
     return plaintext
