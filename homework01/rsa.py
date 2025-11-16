05aa9be1e3884aec9af225f0be68c607d5703e1c
unknown <Nastenkla@gmail.com>
2025-11-16 20:14:25 +0300

    Реализована функция gcd()

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -28,7 +28,10 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    pass
+
+    while b:
+        a, b = b, a % b
+    return a
 
 def multiplicative_inverse(e: int, phi: int) -> int:
     """
